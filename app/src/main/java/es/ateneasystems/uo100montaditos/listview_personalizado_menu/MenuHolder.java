ded3c72572f262f0925f3a20a1da759f5d783b33
Cubel89 <p.cubel@gmail.com>
2015-07-22 17:22:56 +0200

    Prueba de rendimiento con publicidad

---
+++ app/src/main/java/es/ateneasystems/uo100montaditos/listview_personalizado_menu/MenuHolder.java
@@ -0,0 +1,12 @@
+package es.ateneasystems.uo100montaditos.listview_personalizado_menu;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by cubel on 22/07/15.
+ */
+public class MenuHolder {
+    public ImageView ico_item;
+    public TextView tv_1, tv_2;
+}
